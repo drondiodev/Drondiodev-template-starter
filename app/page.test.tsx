e250f45f85141ee9d56362850b4a4e745611f407
David Rondio <103181452+drondiodev@users.noreply.github.com>
2024-07-22 21:54:01 +0200

    add vitest

---
+++ app/page.test.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { render, screen } from '@testing-library/react'
+import { expect, test } from 'vitest'
+
+import Page from './page'
+
+test('Page', () => {
+  render(<Page />)
+  expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
+})
---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+  },
+})
