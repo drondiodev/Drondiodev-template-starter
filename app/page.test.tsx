bf0ef40cf84416d0a2df112275615f2ddcb61acd
David Rondio <103181452+drondiodev@users.noreply.github.com>
2024-07-22 22:00:22 +0200

    feat: add Vitest \& CI/CD Pipeline and Github Actions

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -6,5 +6,7 @@ import Page from './page'
 
 test('Page', () => {
   render(<Page />)
-  expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
+  expect(
+    screen.getByRole('heading', { level: 1, name: 'Next.js Starter template' })
+  ).toBeDefined()
 })
